3570eb3b47fbdb0b12be08ce12312f1000f574ef
Eduardo Guzman Lau Len <eduardo.laulen@nzxt.com>
2022-06-07 18:29:09 -0400

    feat: rename repository and setup eslint/prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'auto',
+};
